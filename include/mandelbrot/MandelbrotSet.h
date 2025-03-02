f6bb14b99c0de2448e946e4c53e6cfa2a6996bb9
arendellian13 <listener1381@outlook.com>
2025-03-02 11:05:22 +0800

    feat: implement MandelbrotSet class with configuration methods and OpenCV integration

--- include/mandelbrot/MandelbrotSet.h
+++ include/mandelbrot/MandelbrotSet.h
@@ -5,4 +5,48 @@
 #ifndef MANDELBROTSET_INCLUDE_MANDELBROT_MANDELBROTSET_H
 #define MANDELBROTSET_INCLUDE_MANDELBROT_MANDELBROTSET_H
 
-#endif //MANDELBROTSET_INCLUDE_MANDELBROT_MANDELBROTSET_H
+#include <complex>
+#include <opencv2/core/mat.hpp>
+
+namespace Mandelbrot {
+
+    class MandelbrotSet {
+    public:
+        constexpr static size_t MAX_ITERATIONS = 1000;
+        constexpr static double ESCAPE_RADIUS = 2.0;
+
+        MandelbrotSet() = default;
+        MandelbrotSet(size_t width, size_t height);
+
+        MandelbrotSet &setWidth(size_t width);
+        MandelbrotSet &setHeight(size_t height);
+        MandelbrotSet &setXMin(double x_min);
+        MandelbrotSet &setXMax(double x_max);
+        MandelbrotSet &setYMin(double y_min);
+        MandelbrotSet &setYMax(double y_max);
+        MandelbrotSet &setResolution(size_t width, size_t height);
+        MandelbrotSet &setXRange(double x_min, double x_max);
+        MandelbrotSet &setYRange(double y_min, double y_max);
+
+        [[nodiscard]] size_t getWidth() const;
+        [[nodiscard]] size_t getHeight() const;
+        [[nodiscard]] double getXMin() const;
+        [[nodiscard]] double getXMax() const;
+        [[nodiscard]] double getYMin() const;
+        [[nodiscard]] double getYMax() const;
+
+#ifdef MANDELBROT_SET_TEST
+        // TODO: Add test helper.
+        friend class MandelbrotSetTest;
+#endif
+
+    private:
+        [[nodiscard]] static size_t compute_escape_time(const std::complex<double> &c) ;
+
+        size_t width_, height_;
+        double x_min_, x_max_, y_min_, y_max_;
+    };
+
+} // namespace Mandelbrot
+
+#endif // MANDELBROTSET_INCLUDE_MANDELBROT_MANDELBROTSET_H
--- src/MandelbrotSet.cpp
+++ src/MandelbrotSet.cpp
@@ -1,3 +1,78 @@
 //
 // Created by Renatus Madrigal on 3/2/2025.
 //
+
+#include "mandelbrot/MandelbrotSet.h"
+
+namespace Mandelbrot {
+    MandelbrotSet::MandelbrotSet(size_t width, size_t height) {
+        width_ = width;
+        height_ = height;
+        x_min_ = -2.0;
+        x_max_ = 2.0;
+        y_min_ = -2.0;
+        y_max_ = 2.0;
+    }
+
+    MandelbrotSet &MandelbrotSet::setWidth(size_t width) {
+        width_ = width;
+        return *this;
+    }
+
+    MandelbrotSet &MandelbrotSet::setHeight(size_t height) {
+        height_ = height;
+        return *this;
+    }
+
+    MandelbrotSet &MandelbrotSet::setXMin(double x_min) {
+        x_min_ = x_min;
+        return *this;
+    }
+
+    MandelbrotSet &MandelbrotSet::setXMax(double x_max) {
+        x_max_ = x_max;
+        return *this;
+    }
+
+    MandelbrotSet &MandelbrotSet::setYMin(double y_min) {
+        y_min_ = y_min;
+        return *this;
+    }
+
+    MandelbrotSet &MandelbrotSet::setYMax(double y_max) {
+        y_max_ = y_max;
+        return *this;
+    }
+
+    MandelbrotSet &MandelbrotSet::setResolution(size_t width, size_t height) {
+        return (this)->setWidth(width).setHeight(height);
+    }
+
+    MandelbrotSet &MandelbrotSet::setXRange(double x_min, double x_max) {
+        return (this)->setXMin(x_min).setXMax(x_max);
+    }
+
+    MandelbrotSet &MandelbrotSet::setYRange(double y_min, double y_max) {
+        return (this)->setYMin(y_min).setYMax(y_max);
+    }
+
+    size_t MandelbrotSet::getWidth() const { return width_; }
+    size_t MandelbrotSet::getHeight() const { return height_; }
+    double MandelbrotSet::getXMin() const { return x_min_; }
+    double MandelbrotSet::getXMax() const { return x_max_; }
+    double MandelbrotSet::getYMin() const { return y_min_; }
+    double MandelbrotSet::getYMax() const { return y_max_; }
+
+    size_t MandelbrotSet::compute_escape_time(const std::complex<double> &c) {
+        std::complex<double> z(0);
+        for (auto i = 0u; i < MAX_ITERATIONS; ++i) {
+            z = z * z + c;
+            if (std::norm(z) > ESCAPE_RADIUS * ESCAPE_RADIUS) {
+                return i;
+            }
+        }
+        return MAX_ITERATIONS;
+    }
+
+
+} // namespace Mandelbrot
