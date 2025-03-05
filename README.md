53373cd14bff06757c314d1441b35c6a7cbca5b1
arendellian13 <listener1381@outlook.com>
2025-03-05 23:31:55 +0800

    feat: add video generation options and enhance command line arguments for Mandelbrot set

--- README.md
+++ README.md
@@ -6,15 +6,14 @@ It was inspired by the math soft course during the Spring 2025 semester at ZJU.
 
 ## Build Pre-requisites
 
-Currently, this project is only tested on Windows with `MSVC` compiler. However, I believe it should work on Linux as
-well.
-
-### Windows
+Currently, this project is only tested on Windows with `MSVC` compiler. The CPU version is also tested on WSL Arch with
+gcc.
 
 - CMake
 - OpenCV
-- MSVC (Clang and GCC should work as well, but I haven't tested it)
-- CUDA (optional)
+- Any modern C++ compiler support OpenMP
+- CUDA (optional) (recommended)
+- MPFR (optional)
 
 ## Build Options
 
@@ -22,14 +21,8 @@ well.
 |-----------------|---------------------------|---------|
 | `ENABLE_CUDA`   | Build with CUDA support   | ON      |
 | `ENABLE_OPENMP` | Build with OpenMP support | ON      |
+| `ENABLE_MPFR`   | Build with MPFR support   | OFF     |
 
 ## Usage
 
-Assuming that the executable is named `mandelbrot`, you can run it with the following command:
-
-```shell
-mandelbrot <width> <height>
-```
-
-Where `<width>` and `<height>` are the dimensions of the output image. Currently, the range of the Mandelbrot set is not
-configurable.
+Assuming that the executable is named `mandelbrot`. You can run `mandelbrot --help` to see the usage.
--- src/main.cpp
+++ src/main.cpp
@@ -17,29 +17,49 @@
 using namespace cv;
 using namespace std;
 
-struct ZoomParams {
-    double xmin, xmax, ymin, ymax;
-};
-
 struct CommandLineArguments {
     size_t width;
     size_t height;
     double x_min, x_max, y_min, y_max;
+    bool video;
     double x_center, y_center, xsize;
-    bool use_center;
+    int max_step;
+    double zoom_factor, scale_rate;
+    bool with_key_frames;
+    bool set_output;
+    string output;
 };
 
+#if ENABLE_CUDA
+using DefaultMandelbrotSet = Mandelbrot::MandelbrotSetCuda;
+constexpr std::string_view CURRENT_IMPLEMENTATION = "CUDA";
+#elif ENABLE_MPFR
+using DefaultMandelbrotSet = Mandelbrot::MandelbrotSetMPFR;
+constexpr std::string_view CURRENT_IMPLEMENTATION = "MPFR";
+#else
+using DefaultMandelbrotSet = Mandelbrot::MandelbrotSet;
+constexpr std::string_view CURRENT_IMPLEMENTATION = "Pure CPU";
+#endif
+
 CommandLineArguments parseArguments(int argc, char **argv_raw) {
-    CommandLineArguments args = {.width = 1024,
-                                 .height = 1024,
-                                 .x_min = -2.0,
-                                 .x_max = 2.0,
-                                 .y_min = -2.0,
-                                 .y_max = 2.0,
-                                 .x_center = 0.0,
-                                 .y_center = 0.0,
-                                 .xsize = 4.0,
-                                 .use_center = false};
+    CommandLineArguments args = {
+            .width = 2048,
+            .height = 2048,
+            .x_min = -2.0,
+            .x_max = 2.0,
+            .y_min = -2.0,
+            .y_max = 2.0,
+            .video = false,
+            .x_center = 0.0,
+            .y_center = 0.0,
+            .xsize = 4.0,
+            .max_step = 10,
+            .zoom_factor = 4.0,
+            .scale_rate = 1.03,
+            .with_key_frames = false,
+            .set_output = false,
+            .output = "",
+    };
     vector<string> argv(argv_raw, argv_raw + argc);
     for (size_t i = 1; i < argc; i++) {
         if (argv[i] == "--resolution") {
@@ -83,21 +103,40 @@ CommandLineArguments parseArguments(int argc, char **argv_raw) {
             args.x_center = std::stod(argv[i + 1]);
             args.y_center = std::stod(argv[i + 2]);
             args.xsize = std::stod(argv[i + 3]);
-            args.use_center = true;
             i += 3;
+        } else if (argv[i] == "--video") {
+            assert(i + 3 < argc);
+            args.video = true;
+            args.max_step = std::stoi(argv[i + 1]);
+            args.zoom_factor = std::stod(argv[i + 2]);
+            args.scale_rate = std::stod(argv[i + 3]);
+            i += 3;
+        } else if (argv[i] == "--with-keyframes") {
+            args.with_key_frames = true;
+        } else if (argv[i] == "-o" || argv[i] == "--output") {
+            assert(i + 1 < argc);
+            args.set_output = true;
+            args.output = argv[i + 1];
+            ++i;
         } else if (argv[i] == "--help") {
-            std::cout << "Usage: " << argv[0] << " [options]" << std::endl;
-            std::cout << "Options:" << std::endl;
-            std::cout << "  --resolution <width> <height>  Set the resolution of the image" << std::endl;
-            std::cout << "  --width <width>                Set the width of the image" << std::endl;
-            std::cout << "  --height <height>              Set the height of the image" << std::endl;
-            std::cout << "  --xmin <xmin>                  Set the minimum x value" << std::endl;
-            std::cout << "  --xmax <xmax>                  Set the maximum x value" << std::endl;
-            std::cout << "  --ymin <ymin>                  Set the minimum y value" << std::endl;
-            std::cout << "  --ymax <ymax>                  Set the maximum y value" << std::endl;
-            std::cout << "  --range <xmin> <xmax> <ymin> <ymax> Set the range of x and y values" << std::endl;
-            std::cout << "  --center <xcenter> <ycenter> <xsize> Set the center and size of the image" << std::endl;
-            std::cout << "  --help                        Display this help message" << std::endl;
+            cout << "A simple Mandelbrot set generator" << endl;
+            cout << "Current implementation: " << CURRENT_IMPLEMENTATION << endl;
+            cout << endl;
+            cout << "Usage: " << argv[0] << " [options]" << endl;
+            cout << "Options:" << endl;
+            cout << "  -o / --output <filename>                       Set the filename of the output file" << endl;
+            cout << "  --resolution <width> <height>                  Set the resolution of the image" << endl;
+            cout << "  --width <width>                                Set the width of the image" << endl;
+            cout << "  --height <height>                              Set the height of the image" << endl;
+            cout << "  --xmin <xmin>                                  Set the minimum x value" << endl;
+            cout << "  --xmax <xmax>                                  Set the maximum x value" << endl;
+            cout << "  --ymin <ymin>                                  Set the minimum y value" << endl;
+            cout << "  --ymax <ymax>                                  Set the maximum y value" << endl;
+            cout << "  --range <xmin> <xmax> <ymin> <ymax>            Set the range of x and y values" << endl;
+            cout << "  --center <xcenter> <ycenter> <xsize>           Set the center and size for video" << endl;
+            cout << "  --video <max_step> <zoom_factor> <scale_rate>  Generate a zooming animation" << endl;
+            cout << "  --with-keyframes                               Generate keyframes for the video" << endl;
+            cout << "  --help                                         Display this help message" << endl;
             exit(0);
         }
     }
@@ -115,48 +154,43 @@ vector<Mat> precomputeScaleMatrices(const Point2f &center, double scale_rate, in
     return scale_matrices;
 }
 
-#if ENABLE_CUDA
-using DefaultMandelbrotSet = Mandelbrot::MandelbrotSetCuda;
-constexpr std::string_view CURRENT_IMPLEMENTATION = "CUDA";
-#elif ENABLE_MPFR
-using DefaultMandelbrotSet = Mandelbrot::MandelbrotSetMPFR;
-constexpr std::string_view CURRENT_IMPLEMENTATION = "MPFR";
-#else
-using DefaultMandelbrotSet = Mandelbrot::MandelbrotSet;
-constexpr std::string_view CURRENT_IMPLEMENTATION = "Pure CPU";
-#endif
-
-int main(int argc, char **argv) {
-    setLogLevel(utils::logging::LogLevel::LOG_LEVEL_SILENT);
-
-    std::cout << "Current implementation: " << CURRENT_IMPLEMENTATION << std::endl;
-
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-
+void generateVideo(const CommandLineArguments &args) {
+    // Require arguments: max_step, zoom_factor, scale_rate, x_center, y_center, xsize, width, height
     // Some constants for the zooming animation. We may make them configurable later.
-    constexpr int MAX_STEP = 40;
-    constexpr double ZOOM_FACTOR = 2.0;
-    constexpr double SCALE_RATE = 1.03;
+    const int max_step = args.max_step;
+    const double zoom_factor = args.zoom_factor;
+    const double scale_rate = args.scale_rate;
 
 
     // Seahorse Valley. We need a more precise center for this.
-    constexpr double xcenter = -0.74525, ycenter = 0.12265;
+    // double xcenter = -0.74525, ycenter = 0.12265;
+    const double xcenter = args.x_center, ycenter = args.y_center;
 
     // Image resolution
-    constexpr int width = 4096, height = 4096;
+    const int width = args.width, height = args.height;
 
-    const int frame_count = ceil(log(ZOOM_FACTOR) / log(SCALE_RATE));
+    auto filename = args.set_output ? args.output : "MandelbrotSet.mp4";
+
+    cout << "Generating video..." << endl;
+    cout << "Resolution: " << width << " x " << height << endl;
+    cout << "Center: " << xcenter << ", " << ycenter << endl;
+    cout << "Max step: " << max_step << endl;
+    cout << fixed << setprecision(2);
+    cout << "Zoom factor: " << zoom_factor << endl;
+    cout << "Scale rate: " << scale_rate << endl;
+
+    const int frame_count = ceil(log(zoom_factor) / log(scale_rate));
 
     DefaultMandelbrotSet mandelbrot_set;
     mandelbrot_set.setResolution(width, height);
 
     VideoWriter writer;
-    writer.open("MandelbrotSetCuda.mp4", VideoWriter::fourcc('h', 'v', 'c', 'l'), 30, Size(width, height), true);
+    writer.open(filename, VideoWriter::fourcc('h', 'v', 'c', 'l'), 30, Size(width, height), true);
     Point2f center(width / 2.0, height / 2.0);
 
 
     std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
-    auto scale_matrices = precomputeScaleMatrices(center, SCALE_RATE, frame_count);
+    const auto scale_matrices = precomputeScaleMatrices(center, scale_rate, frame_count);
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     auto diff = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
 
@@ -164,23 +198,26 @@ int main(int argc, char **argv) {
 
     vector<Mat> frames(frame_count);
 
-    for (uint64_t i = 0, factor = (1 << i); i < MAX_STEP; ++i, factor *= ZOOM_FACTOR) {
+    for (uint64_t i = 0, factor = (1 << i); i < max_step; ++i, factor *= zoom_factor) {
 
-        mandelbrot_set.setCenter(xcenter, ycenter, 4.0 / factor);
+        mandelbrot_set.setCenter(xcenter, ycenter, args.xsize / factor);
 
         start = std::chrono::steady_clock::now();
         const auto image = mandelbrot_set.generate();
         end = std::chrono::steady_clock::now();
         diff = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
-        std::cout << "Time taken to generate frame " << i + 1 << ": " << diff.count() << " seconds" << std::endl;
+        cout << "Time taken to generate frame " << i + 1 << ": " << diff.count() << " seconds" << endl;
 
-        auto filename = std::format("frames/MandelbrotSetCudaFrame{}.png", i + 1);
-        imwrite(filename, image);
 
-        if (i == MAX_STEP - 1)
+        if (args.with_key_frames) {
+            filename = std::format("frames/MandelbrotSetKeyFrame{}.png", i + 1);
+            imwrite(filename, image);
+        }
+
+        if (i == max_step - 1)
             break;
 
-        std::cout << "Start generating frames between " << i + 1 << " and " << i + 2 << std::endl;
+        cout << "Start generating frames between " << i + 1 << " and " << i + 2 << endl;
 
         start = std::chrono::steady_clock::now();
 
@@ -198,66 +235,52 @@ int main(int argc, char **argv) {
         end = std::chrono::steady_clock::now();
         diff = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
 
-        std::cout << "Time taken to generate frames between " << i + 1 << " and " << i + 2 << ": " << diff.count()
-                  << " seconds" << std::endl;
+        cout << "Time taken to generate frames between " << i + 1 << " and " << i + 2 << ": " << diff.count()
+             << " seconds" << endl;
     }
 
     writer.release();
+}
 
-#if 0
-    auto args = parseArguments(argc, argv);
+void generateImage(const CommandLineArguments &args) {
+    DefaultMandelbrotSet mandelbrot_set;
+    mandelbrot_set.setResolution(args.width, args.height);
+    mandelbrot_set.setXRange(args.x_min, args.x_max).setYRange(args.y_min, args.y_max);
 
-    auto width = args.width;
-    auto height = args.height;
-
-    std::chrono::steady_clock::time_point start, end;
-    std::chrono::duration<double> diff{};
-
-#if 0
-    Mandelbrot::MandelbrotSet mandelbrot(width, height);
-
-    mandelbrot.setXRange(zp.xmin, zp.xmax).setYRange(zp.ymin, zp.ymax);
+    cout << fixed << setprecision(2);
+    cout << "Resolution: " << mandelbrot_set.getWidth() << " x " << mandelbrot_set.getHeight() << endl;
+    cout << "XRange: " << mandelbrot_set.getXMin() << " - " << mandelbrot_set.getXMax() << endl;
+    cout << "YRange: " << mandelbrot_set.getYMin() << " - " << mandelbrot_set.getYMax() << endl;
 
     std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
-    const auto image = mandelbrot.generate();
+    const auto image_cuda = mandelbrot_set.generate();
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     auto diff = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
-    std::cout << "Time taken to generate the image: " << diff.count() << " seconds" << std::endl;
+    cout << "Time taken to generate the image using CUDA: " << diff.count() << " seconds" << endl;
 
-    imwrite("MandelbrotSet.png", image);
-#endif
+    auto filename = args.set_output ? args.output : "MandelbrotSetCuda.png";
+    imwrite(filename, image_cuda);
+}
 
+int main(int argc, char **argv) {
+    setLogLevel(utils::logging::LogLevel::LOG_LEVEL_SILENT);
 
-#if ENABLE_CUDA
-    Mandelbrot::MandelbrotSetCuda mandelbrot_set_cuda;
-    mandelbrot_set_cuda.setResolution(width, height);
-    if (args.use_center) {
-        mandelbrot_set_cuda.setCenter(args.x_center, args.y_center, args.xsize);
+    CommandLineArguments args = parseArguments(argc, argv);
+
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+    cout << "Current implementation: " << CURRENT_IMPLEMENTATION << endl;
+
+    if (args.video) {
+        generateVideo(args);
     } else {
-        mandelbrot_set_cuda.setXRange(args.x_min, args.x_max).setYRange(args.y_min, args.y_max);
+        generateImage(args);
     }
-
-    cout << "Resolution: " << mandelbrot_set_cuda.getWidth() << " x " << mandelbrot_set_cuda.getHeight() << endl;
-    cout << fixed << setprecision(2) // NOLINT
-         << "XRange: " << mandelbrot_set_cuda.getXMin() << " - " << mandelbrot_set_cuda.getXMax() << endl;
-    cout << fixed << setprecision(2) // NOLINT
-         << "YRange: " << mandelbrot_set_cuda.getYMin() << " - " << mandelbrot_set_cuda.getYMax() << endl;
-
-    start = std::chrono::steady_clock::now();
-    const auto image_cuda = mandelbrot_set_cuda.generate();
-    end = std::chrono::steady_clock::now();
-    diff = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
-    std::cout << "Time taken to generate the image using CUDA: " << diff.count() << " seconds" << std::endl;
-
-    imwrite("MandelbrotSetCuda.png", image_cuda);
-#endif
-
-#endif
 
     std::chrono::steady_clock::time_point finish = std::chrono::steady_clock::now();
     auto total_time = std::chrono::duration_cast<std::chrono::duration<double>>(finish - begin);
 
-    std::cout << "Total time: " << total_time.count() << " seconds" << std::endl;
+    cout << "Total time: " << total_time.count() << " seconds" << endl;
 
     return 0;
 }
