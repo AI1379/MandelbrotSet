343d278a7a6c65846a5817559ca803d6071c785c
arendellian13 <listener1381@outlook.com>
2025-03-15 22:17:59 +0800

    fix: Improve STDEXEC package handling by checking for STDEXEC_FOUND before setting include directory

--- cmake/FindSTDEXEC.cmake
+++ cmake/FindSTDEXEC.cmake
@@ -23,25 +23,20 @@ if (NOT TARGET STDEXEC::stdexec)
     )
     set(STDEXEC_INCLUDE_DIR ${STDEXEC_SOURCE_DIR}/include)
     message(STATUS "stdexec include dir: ${STDEXEC_INCLUDE_DIR}")
-    if (STDEXEC_INCLUDE_DIR)
-        file(TO_CMAKE_PATH ${STDEXEC_INCLUDE_DIR} STDEXEC_INCLUDE_DIR)
-    else ()
-        message(FATAL_ERROR "stdexec not found")
-    endif ()
-
     include(FindPackageHandleStandardArgs)
     find_package_handle_standard_args(
             STDEXEC
-            REQUIRED_VARS STDEXEC_INCLUDE_DIR
-            FOUND_VAR STDEXEC_FOUND
-            VERSION_VAR STDEXEC_VERSION
+            REQUIRED_VARS STDEXEC_SOURCE_DIR
             FAIL_MESSAGE "stdexec not found"
     )
+    if (STDEXEC_FOUND)
+        file(TO_CMAKE_PATH ${STDEXEC_INCLUDE_DIR} STDEXEC_INCLUDE_DIR)
+        add_library(STDEXEC::stdexec INTERFACE IMPORTED)
+        target_include_directories(
+                STDEXEC::stdexec SYSTEM INTERFACE ${STDEXEC_INCLUDE_DIR}
+        )
 
-    add_library(STDEXEC::stdexec INTERFACE IMPORTED)
-    target_include_directories(
-            STDEXEC::stdexec SYSTEM INTERFACE ${STDEXEC_INCLUDE_DIR}
-    )
+        mark_as_advanced(STDEXEC_INCLUDE_DIR STDEXEC_ROOT)
+    endif ()
 
-    mark_as_advanced(STDEXEC_INCLUDE_DIR STDEXEC_ROOT)
 endif ()
